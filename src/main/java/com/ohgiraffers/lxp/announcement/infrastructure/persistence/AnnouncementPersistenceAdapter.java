1375bc927b54db7eee05575f8de15b02e6693c91
yerim <na990303@gmail.com>
2026-06-23 18:20:09 +0900

    AnnouncementPersistenceAdapter @Transactional 추가

--- src/main/java/com/ohgiraffers/lxp/announcement/infrastructure/persistence/AnnouncementPersistenceAdapter.java
+++ src/main/java/com/ohgiraffers/lxp/announcement/infrastructure/persistence/AnnouncementPersistenceAdapter.java
@@ -5,6 +5,7 @@ import com.ohgiraffers.lxp.announcement.domain.model.entity.Announcement;
 import com.ohgiraffers.lxp.announcement.infrastructure.persistence.jpa.AnnouncementJpaEntity;
 import com.ohgiraffers.lxp.announcement.infrastructure.persistence.jpa.AnnouncementRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class AnnouncementPersistenceAdapter implements SaveAnnouncementPort {
@@ -16,6 +17,7 @@ public class AnnouncementPersistenceAdapter implements SaveAnnouncementPort {
     }
 
     @Override
+    @Transactional
     public Announcement save(Announcement announcement) {
         AnnouncementJpaEntity entity = AnnouncementMapper.toJpaEntity(announcement);
         AnnouncementJpaEntity saved = announcementRepository.save(entity);
