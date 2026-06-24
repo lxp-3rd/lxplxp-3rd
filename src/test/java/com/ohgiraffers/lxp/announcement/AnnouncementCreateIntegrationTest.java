ed52e34b966e2737404483eb584cb851401e706a
ssggii <haan7525@gmail.com>
2026-06-24 10:54:26 +0900

    test: dev 시드와 충돌하던 통합테스트의 시드 로딩 비활성화 #20

--- src/test/java/com/ohgiraffers/lxp/announcement/AnnouncementCreateIntegrationTest.java
+++ src/test/java/com/ohgiraffers/lxp/announcement/AnnouncementCreateIntegrationTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("dev")
+@TestPropertySource(properties = "spring.sql.init.mode=never")
 class AnnouncementCreateIntegrationTest {
 
     @Autowired
--- src/test/java/com/ohgiraffers/lxp/enrollment/EnrollmentCancelIntegrationTest.java
+++ src/test/java/com/ohgiraffers/lxp/enrollment/EnrollmentCancelIntegrationTest.java
@@ -14,12 +14,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("dev")
+@TestPropertySource(properties = "spring.sql.init.mode=never")
 @Transactional
 @DisplayName("수강취소 통합 테스트")
 class EnrollmentCancelIntegrationTest {
